e7a522ad1acc55ba4cfc1d433e439c90f6ecf930
veggiedefender <jessejesse123@gmail.com>
2018-01-28 15:53:18 -0500

    fix shellcheck issues

--- get_code.sh
+++ get_code.sh
@@ -1,7 +1,9 @@
+#!/usr/bin/env bash
+
 printf 'Make sure Steam Guard Mobile Authenticator is DISABLED or you will be
 locked out of your Steam account!
 '
-read -p 'Press [ENTER] when you are ready, or Ctrl-C to exit.'
+read -rp 'Press [ENTER] when you are ready, or Ctrl-C to exit.'
 
 printf '[ Pulling APK from device ]\n'
 adb pull /data/app/com.valvesoftware.android.steam.community-1/base.apk steam.apk
@@ -17,14 +19,14 @@ printf '\n[ Rebuilding APK ]\n'
 apktool b steam
 
 printf '\n[ Generating signing key ]\n'
-STOREPASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-KEYPASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+STOREPASS=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 32 | head -n 1)
+KEYPASS=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 32 | head -n 1)
 keytool -genkey -noprompt \
     -keyalg RSA \
     -keysize 2048 \
     -validity 10000 \
-    -storepass $STOREPASS \
-    -keypass $KEYPASS \
+    -storepass "$STOREPASS" \
+    -keypass "$KEYPASS" \
     -keystore key.keystore \
     -alias alias \
     -dname "CN=example.com, OU=dont, O=use, L=this, S=in, C=production"
@@ -34,8 +36,8 @@ jarsigner \
     -sigalg SHA1withRSA \
     -digestalg SHA1 \
     -keystore key.keystore \
-    -storepass $STOREPASS \
-    -keypass $KEYPASS \
+    -storepass "$STOREPASS" \
+    -keypass "$KEYPASS" \
     steam/dist/steam.apk \
     alias
 rm key.keystore
@@ -50,7 +52,7 @@ adb shell monkey -p com.valvesoftware.android.steam.community 1
 printf '
 Sign in to Steam and ENABLE Steam Guard Mobile Authenticator.
 '
-read -p 'Press [ENTER] when you are ready.'
+read -rp 'Press [ENTER] when you are ready.'
 
 printf '
 Extracting data. Please confirm "back up my data" on device. DO NOT set a password.
